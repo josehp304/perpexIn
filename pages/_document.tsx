8c23f949405ea233585e4dd7e679dc141b97c628
Raghav Krishna <raghav@Raghavs-MacBook-Air.local>
2025-07-24 19:40:37 +0530

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <title>Perpex Insights</title>
-      <Head />
+      <Head>
+        <title>Perpex Insights</title>
+        {/* Google Analytics Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WHQZRH1KB"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8WHQZRH1KB');
+          `
+        }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
